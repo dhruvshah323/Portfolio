5e1126ccef547bc54fbff9cd3ba06e30ddef3135
dhruvshah323 <77463252+dhruvshah323@users.noreply.github.com>
2022-03-14 10:30:07 -0400

    Added Responsive to landing page

---
+++ scripts/script.js
@@ -0,0 +1,12 @@
+// var messageArray = ["I'm DHruv"]
+// var textPosition = 0;
+// var speed = 100;
+
+// typewritter = () =>{
+//     document.querySelector('#typerWritter').innerHTML = messageArray[0].substring(0, textPosition) + "<span>\u25ae</span>";
+
+//     if(textPosition++ != messageArray[0].length)
+//         setTimeout(typewritter. speed);
+// }
+
+// window.addEventListener("load", typewritter);
